b84d38aed2a95d43c52666e9dff3f0c46cfdcb8a
jagruth23 <108266459+jagruth23@users.noreply.github.com>
2023-04-26 18:18:03 +0530

    Add files via upload

---
+++ success.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Success</title>
+</head>
+<body>
+	<h2>Thank You</h2>
+	<p>Thank you for filling out the form <?php echo $_POST['name']; ?>.</p>
+</body>
+</html>
